4dfeef1fd7147ef9fff887c871bebb28a21d537e
gugachadg <akolashviliguga1@gmail.com>
2025-02-11 20:33:34 +0400

    first commit

---
+++ day 01/classwork/classwork.py
@@ -0,0 +1,21 @@
+name="guga"
+surname="akolashvili"
+age=11
+
+height=135.9
+
+KNOWS_PROGRAMMING=True
+programming=KNOWS_PROGRAMMING
+
+is_ugly=False
+ 
+print(name+"   "+surname)
+
+
+print(name)
+
+print(age)
+print(name)
+print(surname)
+print(height)
+print(KNOWS_PROGRAMMING)
