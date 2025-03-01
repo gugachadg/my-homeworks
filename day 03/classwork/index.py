e41c9bf454caa5dc7a51c55aaa26e93f407b2c35
gugachadg <akolashviliguga1@gmail.com>
2025-03-01 14:12:05 +0400

    third commit

---
+++ day 03/classwork/index.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter your first number"))
+num2 = int(input("enter your second number"))
+if num1 == num2:
+    print("they are equal")
+elif num1 > num2:
+    print("first number is higher")
+elif num2 > num1:
+    print("second number is higher")
